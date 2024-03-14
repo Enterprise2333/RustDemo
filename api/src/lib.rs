63b449ea6eacf3e85f42090892597c2b2e116f56
Enterprise2333 <Chester20020928@gmail.com>
2024-03-14 22:56:12 +0800

    将服务移植到api模块中启动

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use axum::{
+    routing::get,
+    Router,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[tokio::main]
+pub async fn start() {
+    
+    //获取port值
+    let port = std::env::var("PORT").expect("PORT is not set in file .env");
+    
+    println!("端口为{}", port);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{port}")).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
